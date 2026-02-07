110dc0d72d8f0bb31b84ebea7182df208a180b31
Tyson <tyson.granger181@gmail.com>
2026-02-08 02:40:26 +0530

    update configuration to load sensitive data from environment variables and add example config file

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 import yaml
 import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 CONFIG_FILE = 'config.yaml'
 
 def load_config():
     try:
         with open(CONFIG_FILE, 'r') as f:
-            return yaml.safe_load(f)
+            config = yaml.safe_load(f)
+            config['telegram']['bot_token'] = os.getenv('TELEGRAM_BOT_TOKEN')
+            config['telegram']['chat_id'] = os.getenv('TELEGRAM_CHAT_ID')
+            return config
     except FileNotFoundError:
         logging.error(f"Configuration file {CONFIG_FILE} not found.")
         return None
